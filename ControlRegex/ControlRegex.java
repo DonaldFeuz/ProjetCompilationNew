7c9e2d67244dec46a8a3fe9067ead79ca62df7f1
ngwanodimitri <ngwanodimitri@gmail.com>
2021-11-10 09:04:02 +0100

    verifie regex bien former

--- ControlRegex/ControlRegex.java
+++ ControlRegex/ControlRegex.java
@@ -15,35 +15,95 @@ public class ControlRegex {
 
     private String Regex;
     private String[] AlphabetRegex;
+    private String[] Operateur;
 
-    //cette methode suivante nous permetra de savoir si un regex est bien former et envoyer un message d'erreur aux cas contraire
-    public boolean VerifierQueLeRegexEstBienFormer(String Regex) {
+   // cette methode verifie si un charactere est une lettre de l'alphabet ou pas
+   // public boolean VerifierQueLeCharactereEstUneLettreOuUnPas(char lettre) {
+   //     if ((lettre >= 'A' && lettre <= 'Z') || (lettre >= 'a' && lettre <= 'z'))
+   //         return true;
+   //     return false;
+   // }
 
+    // cette methode verifie si un charactere est un operateur ou pas
+    public boolean VerifierQueLeCharactereEstUnOperateurOuUnPas(char Operateur) {
+        if (Operateur == '+' || Operateur == '*' || Operateur == '|')
+            return true;
         return false;
     }
 
-    //cette methode suivante nous permetra de savoir si un regex est bien parenthèser et envoyer un message d'erreur aux cas contraire 
+    // cette methode teste si le texte saisir ne contient que les operateur et les
+    // lettres
+    public boolean ContientLettreEtOperateur(String Regex) {
+        int cpt = 0;
+        for (int i = 0; i < Regex.length(); i++) {
+            if (!((Regex.charAt(i) >= 'A' && Regex.charAt(i) <= 'Z')
+                    || (Regex.charAt(i) >= 'a' && Regex.charAt(i) <= 'z')
+                    || (Regex.charAt(i) == '+' || Regex.charAt(i) == '*' || Regex.charAt(i) == '|')
+                    || (Regex.charAt(i) == '0' || Regex.charAt(i) == '1' || Regex.charAt(i) == '2'
+                            || Regex.charAt(i) == '3' || Regex.charAt(i) == '4' || Regex.charAt(i) == '5'
+                            || Regex.charAt(i) == '6' || Regex.charAt(i) == '7' || Regex.charAt(i) == '8'
+                            || Regex.charAt(i) == '9'))) {
+                cpt++;
+            }
+        }
+        if (cpt == 0)
+            return true;
+        return false;
+    } 
+
+    // cette methode suivante nous permetra de savoir si un regex est bien former et
+    // envoyer un message d'erreur aux cas contraire
+    public boolean VerifierQueLeRegexEstBienFormer(String Regex) {
+        int cpt = 0;
+        if (ContientLettreEtOperateur(Regex) == true) {
+            if (VerifierQueLeCharactereEstUnOperateurOuUnPas(Regex.charAt(Regex.length() - 1)) == true
+                    || VerifierQueLeCharactereEstUnOperateurOuUnPas(Regex.charAt(0)) == true) {
+                return false;
+            } else {
+                for (int i = 1; i < Regex.length(); i++) {
+                    if (VerifierQueLeCharactereEstUnOperateurOuUnPas(Regex.charAt(i - 1)) == true
+                            && VerifierQueLeCharactereEstUnOperateurOuUnPas(Regex.charAt(i)) == true) {
+                        cpt++;
+                    }
+                }
+                if (cpt == 0) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+
+        } else {
+            return false;
+        }
+
+    }
+
+    // cette methode suivante nous permetra de savoir si un regex est bien
+    // parenthèser et envoyer un message d'erreur aux cas contraire
     public boolean VerifierQueLeRegexEstBienParentheser(String Regex) {
 
         return false;
     }
-    
-    
-    //cette methode suivante nous permetra de recupérer automatiquement l'alphabet du regex entrer par l'utilisateur .  
+
+    // cette methode suivante nous permetra de recupérer automatiquement l'alphabet
+    // du regex entrer par l'utilisateur .
     public String[] RecupérationAlphabetDuRegex(String Regex) {
 
         return null;
     }
-    
-    //cette methode suivante nous permetra de transformer un regex infixé sous forme postfixé.  
+
+    // cette methode suivante nous permetra de transformer un regex infixé sous
+    // forme postfixé.
     public String TransformeUnRegexSousFormePostfixe(String Regex) {
 
         return null;
     }
-     //cette methode suivante nous permetra de construire l'ensemble des transitions du regex.  
+
+    // cette methode suivante nous permetra de construire l'ensemble des transitions
+    // du regex.
     public String ConstruireTransitionAFN(String Regex) {
 
-    
-         return null;
+        return null;
     }
 }
--- main/Main.java
+++ main/Main.java
@@ -5,6 +5,8 @@
  */
 package main;
 
+import ControlRegex.ControlRegex;
+
 /**
  *
  * @author donald
@@ -12,21 +14,24 @@ package main;
 public class Main {
 
     /**
-     * cette classe vas nous permettre de recuperer une regex et d'éffectuer toute les taches qui sont associés 
-     * tout commence ici!!! beaucoup de courage et de concentration à tous !!!!
+     * cette classe vas nous permettre de recuperer une regex et d'éffectuer toute
+     * les taches qui sont associés tout commence ici!!! beaucoup de courage et de
+     * concentration à tous !!!!
+     * 
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       
-        System.out.println("ca deja commencer !!");
+        ControlRegex c= new ControlRegex();
+
+        System.out.println(c.VerifierQueLeRegexEstBienFormer("00m+8a+m+a\n"));
         // VerifierQueLeRegexEstBienFormer(Regex);
         // VerifierQueLeRegexEstBienParentheser(Regex);
         // RecupérationAlphabetDuRegex(Regex);
         // TransformeUnRegexSousFormePostfixe();
         // ConstruireTransitionAFN();
         // ConstruireTransitionAFD();
-         
+        
+
     }
-    
-    
+
 }
