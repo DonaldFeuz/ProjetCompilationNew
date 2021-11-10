5a8f1fe5ae0100204bec83681772802d1588f9fc
franckngakou <franckngakou@gmail.com>
2021-11-10 06:50:07 +0100

    recuperation de l'alphabet terminer

--- ControlRegex/ControlRegex.java
+++ ControlRegex/ControlRegex.java
@@ -16,34 +16,48 @@ public class ControlRegex {
     private String Regex;
     private String[] AlphabetRegex;
 
-    //cette methode suivante nous permetra de savoir si un regex est bien former et envoyer un message d'erreur aux cas contraire
+    // cette methode suivante nous permetra de savoir si un regex est bien former et
+    // envoyer un message d'erreur aux cas contraire
     public boolean VerifierQueLeRegexEstBienFormer(String Regex) {
 
         return false;
     }
 
-    //cette methode suivante nous permetra de savoir si un regex est bien parenthèser et envoyer un message d'erreur aux cas contraire 
+    // cette methode suivante nous permetra de savoir si un regex est bien
+    // parenthèser et envoyer un message d'erreur aux cas contraire
     public boolean VerifierQueLeRegexEstBienParentheser(String Regex) {
 
         return false;
     }
-    
-    
-    //cette methode suivante nous permetra de recupérer automatiquement l'alphabet du regex entrer par l'utilisateur .  
-    public String[] RecupérationAlphabetDuRegex(String Regex) {
 
-        return null;
+    // cette methode suivante nous permetra de recupérer automatiquement l'alphabet
+    // du regex entrer par l'utilisateur .
+    public String RecupérationAlphabetDuRegex(String Regex) {
+        String resut = new String();
+        for (int i = 0; i < Regex.length(); i++) {
+            if ((Regex.charAt(i) >= 'A' && Regex.charAt(i) <= 'Z') || (Regex.charAt(i) >= 'a' && Regex.charAt(i) <= 'z')
+                    || (Regex.charAt(i) >= '0' && Regex.charAt(i) <= '9')) {
+                if (!resut.contains(Character.toString(Regex.charAt(i)))) {
+                    resut = resut + " " + Character.toString(Regex.charAt(i));
+
+                }
+            }
+
+        }
+        return resut;
     }
-    
-    //cette methode suivante nous permetra de transformer un regex infixé sous forme postfixé.  
+
+    // cette methode suivante nous permetra de transformer un regex infixé sous
+    // forme postfixé.C
     public String TransformeUnRegexSousFormePostfixe(String Regex) {
 
         return null;
     }
-     //cette methode suivante nous permetra de construire l'ensemble des transitions du regex.  
+
+    // cette methode suivante nous permetra de construire l'ensemble des transitions
+    // du regex.
     public String ConstruireTransitionAFN(String Regex) {
 
-    
-         return null;
+        return null;
     }
 }
